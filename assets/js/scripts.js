2ad4f6ce1135306f98ddaaf1a888e447b4a5b2ba
Josef Kolář <thejoeejoee@gmail.com>
2019-01-27 19:53:55 +0100

    Remove fbclid

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,33 @@
 // A $( document ).ready() block.
-$( document ).ready(function() {
+$(document).ready(function () {
 
-	// DropCap.js
-	var dropcaps = document.querySelectorAll(".dropcap");
-	window.Dropcap.layout(dropcaps, 2);
+    // DropCap.js
+    var dropcaps = document.querySelectorAll(".dropcap");
+    window.Dropcap.layout(dropcaps, 2);
 
-	// Responsive-Nav
-	var nav = responsiveNav(".nav-collapse");
+    // Responsive-Nav
+    var nav = responsiveNav(".nav-collapse");
 
-	// Round Reading Time
+    // Round Reading Time
     $(".time").text(function (index, value) {
-      return Math.round(parseFloat(value));
+        return Math.round(parseFloat(value));
     });
 
 });
 
-
+(function () {
+    var param = 'fbclid';
+    if (window.location.search.indexOf(param + '=') !== -1) {
+        var replace = '';
+        try {
+            var url = new URL(window.location);
+            url.searchParams.delete(param);
+            replace = url.pathname + url.search + url.hash;
+        } catch (ex) {
+            var regExp = new RegExp('[?&]' + param + '=.*$');
+            replace = window.location.search.replace(regExp, '');
+            replace = window.location.pathname + replace + window.location.hash;
+        }
+        history.replaceState(null, '', replace);
+    }
+})();
